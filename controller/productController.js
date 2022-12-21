d8f262cca1516bb83db6347f6666e4f30a088bfd
Rafael Luiz Batistoti <97912202+RafaelBatistoti@users.noreply.github.com>
2022-12-21 14:40:00 -0300

    Criação de novo produto

--- controller/productController.js
+++ controller/productController.js
@@ -1,8 +1,14 @@
 const expressAsyncHandler = require('express-async-handler')
 const Product = require('../models/productModel')
 
-const createProduct = expressAsyncHandler(async(req,res)=>{
-    res.json('Product posted!!')
+const createProduct = expressAsyncHandler(async (req, res) => {
+
+    try {
+        const newProduct = await Product.create(req.body)
+        res.json(newProduct)
+    } catch (err) {
+        throw new Error(`Was not possible create the product. Check the message: ${err}`)
+    }
 })
 
 module.exports = {
